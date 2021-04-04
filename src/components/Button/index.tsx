e24c683be4fa014fe8e111fe5fadfd16627b51f0
禹賢祐 <whw3312@gmail.com>
2021-04-04 18:44:47 +0900

    no message

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
 export interface ButtonProps{
   label?: string
   backgroundColor?: string
   onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void
 }
 
-const Btn = styled.button<ButtonProps>`
-  background-color: #000;
-`
-
 export const Button: React.FC<ButtonProps> = ({
   label,
   onClick
 }) => {
   return (
-    <Btn onClick={onClick}>{label}</Btn>
+    <button onClick={onClick}>{label}</button>
   )
 }
 
--- src/stories/button/Button.stories.d.ts
+++
@@ -1,4 +0,0 @@
-import { Story, Meta } from '@storybook/react';
-declare const _default: Meta<import("@storybook/react").Args>;
-export default _default;
-export declare const Primary: Story<import("@storybook/react").Args>;
