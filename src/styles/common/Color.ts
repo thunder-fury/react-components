238a8874102f14d64c4f7639fe3e0f0c893132d8
woohyeonwoo <>
2022-04-08 19:43:35 +0900

    カラー

--- src/styles/common/Color.ts
+++ src/styles/common/Color.ts
@@ -1,13 +1,14 @@
 export const Color = {
-  primary: '#34b4e3',
+  primary: `#34b4e3`,
 
-  error: '#e3347d',
+  error: `#e3347d`,
 
-  success: '#4ce2a7',
+  success: `#4ce2a7`,
 
-  dark: '#132e54',
-
-  white: '#fff',
-  black: '#000',
-  gray: '#f2f2f2'
+  dark: `#132e54`,
+  darkBlack: `#222227`,
+  white: `#fff`,
+  black: `#000`,
+  gray: `#f2f2f2`,
+  gray2: `#9c9c9c`,
 }
